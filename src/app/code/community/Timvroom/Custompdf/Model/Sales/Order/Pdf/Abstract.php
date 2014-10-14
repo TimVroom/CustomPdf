f468e78362f93586e37ff6cda1ad310ca29390c6
Tim Vroom <tim@nichecommerce.nl>
2014-10-14 15:11:31 +0200

    Initial commit

---
+++ src/app/code/community/Timvroom/Custompdf/Model/Sales/Order/Pdf/Abstract.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * NicheCommerce
+ *
+ * @category    NicheCommerce
+ * @package     NicheCommerce
+ * @copyright   Copyright (c) 2012 NicheCommerce. (http://nichecommerce.nl)
+ * @author      Tim Vroom (tim@nichecommerce.nl)
+ */
