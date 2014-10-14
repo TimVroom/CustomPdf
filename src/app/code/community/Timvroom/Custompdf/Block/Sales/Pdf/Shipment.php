c2a7583ff0d866e5ab545cbac23378e13bb4e112
Tim Vroom <tim@nichecommerce.nl>
2014-10-14 15:24:38 +0200

    Minor setup for templates and typos

--- src/app/code/community/Timvroom/Custompdf/Block/Sales/Pdf/Shipment.php
+++ src/app/code/community/Timvroom/Custompdf/Block/Sales/Pdf/Shipment.php
@@ -7,7 +7,7 @@
  * @copyright   Copyright (c) 2012 NicheCommerce. (http://nichecommerce.nl)
  * @author      Tim Vroom (tim@nichecommerce.nl)
  */
-abstract class Timvroom_Custompdf_Model_Sales_Order_Pdf_Abstract extends Mage_Sales_Model_Order_Pdf_Abstract
+class Timvroom_Custompdf_Block_Sales_Pdf_Shipment extends Timvroom_Custompdf_Block_Sales_Pdf_Abstract
 {
 
 }
--- src/app/code/community/Timvroom/Custompdf/Model/Sales/Order/Pdf/Shipment.php
+++ src/app/code/community/Timvroom/Custompdf/Model/Sales/Order/Pdf/Shipment.php
@@ -7,7 +7,10 @@
  * @copyright   Copyright (c) 2012 NicheCommerce. (http://nichecommerce.nl)
  * @author      Tim Vroom (tim@nichecommerce.nl)
  */
-abstract class Timvroom_Custompdf_Model_Sales_Order_Pdf_Abstract extends Mage_Sales_Model_Order_Pdf_Abstract
+class Timvroom_Custompdf_Model_Sales_Order_Pdf_Shipment extends Timvroom_Custompdf_Model_Sales_Order_Pdf_Abstract
 {
+    public function getPdf()
+    {
 
+    }
 }
